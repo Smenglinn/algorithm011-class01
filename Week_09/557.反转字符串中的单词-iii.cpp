8078fbed52e1bd7acb79a1eb6c1afb7dda1bc3a5
Smenglin <sheng_menglin@163.com>
2020-08-23 18:31:18 +0800

    week09_assignment

---
+++ Week_09/557.反转字符串中的单词-iii.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=557 lang=cpp
+ *
+ * [557] 反转字符串中的单词 III
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reverseWords(string s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != ' ') {   // when i is a non-space
+                int j = i;
+                for (; j < s.length() && s[j] != ' '; j++) { } // move j to the next space
+                reverse(s.begin() + i, s.begin() + j);
+                i = j - 1;
+            }
+        }
+        return s;
+    }
+};
+// @lc code=end
+
